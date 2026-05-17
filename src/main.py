f95cb8c979e5987cf0b4abc546667b9940ea302f
Jason <jasonkwok475@gmail.com>
2026-05-17 10:16:45 -0700

    Updated new default nodes

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ from config.settings import *
 
 # Mock data
 root_data = TreeNode("Helios", "root", [
-    TreeNode(HELIOS_CORE_CONTAINER, "main", location="https://github.com/helios-data/helios-core", type=Node_Type['GITHUB'], hash="latest"),
+    TreeNode(HELIOS_CORE_CONTAINER, "main", location="https://github.com/helios-data/helios-core", type=Node_Type.GITHUB, hash="latest"),
     TreeNode("FALCON", "1", [
-        TreeNode("Telemetry", "2", location="https://github.com/UBC-Rocket/helios-cots-telemetry", type=Node_Type['GITHUB'], hash="latest"),
+        TreeNode("Telemetry", "2", location="https://github.com/UBC-Rocket/helios-cots-telemetry", type=Node_Type.GITHUB, hash="latest"),
     ]),
     TreeNode("Services", "6", [
-      TreeNode("Livestreaming", "7", location="https://github.com/helios-data/helios-livestreaming", type=Node_Type['GITHUB'], hash="46daf4ebebc70dea5e4560999428f99b271f2659")
+      TreeNode("Dashboard", "7", location="https://github.com/helios-data/helios-dashboard", type=Node_Type.GITHUB, hash="latest"),
+      TreeNode("Livestreaming", "8", location="https://github.com/helios-data/helios-livestreaming", type=Node_Type.GITHUB, hash="latest"),
     ]),
 ])
 
